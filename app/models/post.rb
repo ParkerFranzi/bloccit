8a02b9fea75be4a5381021c882dc098d81f7dd77
Parker <parker.franzi@gmail.com>
2013-11-24 17:08:20 -0800

    Help

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
   attr_accessible :body, :title
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  has_many :posts
 end
