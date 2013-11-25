6531691b21ea78bbbb7989f9b350b03fa1a1ddc9
Parker <parker.franzi@gmail.com>
2013-11-25 14:07:29 -0800

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,18 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
     @post = Post.new(params[:post])
     authorize! :create, @post, message: "You need to be signed up to do that."
@@ -24,12 +24,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(params[:post])
