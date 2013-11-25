52e9911af10e5a241b43167b0a34e879892c0710
Parker <parker.franzi@gmail.com>
2013-11-25 01:08:03 -0800

    Implemented Cancan and basic roles

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jquery-rails'
 gem 'faker'
 gem 'newrelic_rpm'
 gem 'devise'
+gem 'cancan'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |excpetion|
+    redirect_to root_url, :alert => excpetion.message
+  end
 end
