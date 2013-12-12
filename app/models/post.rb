9497b087ec28a701af03e8f4f0ad88395b2ae80a
Parker <parker.franzi@gmail.com>
2013-12-11 17:52:04 -0800

    testing topic show

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,7 +25,8 @@ class Post < ActiveRecord::Base
   end
 
   default_scope order('created_at DESC')
-
+  scope :visible_to, lambda { |user| user ? scoped : joins(:topic).where('topics.public' => true) }
+  
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
