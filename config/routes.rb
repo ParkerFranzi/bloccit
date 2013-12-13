d74b5c04085af6cf84bb1d7938a54bfe7544a0d5
Parker <parker.franzi@gmail.com>
2013-12-12 22:47:50 -0800

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
+
+  resources :posts, only: [:index]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
@@ -11,7 +14,7 @@ Bloccit::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   
   match "about" => 'welcome#about', via: :get
 
